f222a37b88b2cee7266c2b7f1488a0f3aa51327d
Daniel Brandt-Olsen <danbr@itu.dk>
2020-09-10 20:20:38 +0200

    adding tweenmax

--- custom.js
+++ custom.js
@@ -100,3 +100,29 @@ var app = {
 }
 
 app.init();
+
+const boxes = document.querySelectorAll('.item');
+
+const config = {
+  threshold: 0.5 };
+
+const tl = new TimelineMax();
+
+let observer = new IntersectionObserver(function (entries, self) {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      let overlap = '-=0.3';
+
+      if (!tl.isActive()) {
+        overlap = '+=0';
+      }
+
+      tl.to(entry.target, 0.5, { autoAlpha: 1 }, overlap);
+      self.unobserve(entry.target);
+    }
+  });
+}, config);
+
+boxes.forEach(box => {
+  observer.observe(box);
+});
